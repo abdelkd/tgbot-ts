df07931b579bb4993711b3fc9d58b21f6da5fa67
Abdelkarim Djelalda <abdelkarimdjelalda@protonmail.com>
2022-11-26 20:30:03 +0100

    add some enhancements

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Bot } from "grammy";
 
+import dotenv from 'dotenv';
+dotenv.config()
 
-let BOT_TOKEN = process.env.BOT_TOKEN as string
+let BOT_TOKEN: string = process.env.BOT_TOKEN!
+
 const bot = new Bot(BOT_TOKEN)
-
 console.log('The Bot is Now Running...')
-
 
 bot.command('start', (ctx) => {
   ctx.reply('Welcome! send me a text and i\'ll reply back with same text')
